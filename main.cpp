add790627354b87d22f4b42b10eac3cce9363075
Dlucky2212 <paolo.pinto@ucsp.edu.pe>
2017-06-29 19:03:23 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include "Game.h"
+
+using namespace std;
+
+int main()
+{
+    Game pok;
+    pok.Start();
+    return 0;
+}
